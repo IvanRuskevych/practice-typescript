ba8fc6b4132782cb9eb90cf3ae9f8bc92c0b250a
Ivan <ivan.ruskevych@gmail.com>
2023-10-08 16:05:33 +0300

    Revert "Less 003"

--- less-003--class/src/app1.ts
+++
@@ -1,7 +0,0 @@
-console.log('Lesson 003 app1');
-
-const app = document.getElementById('app');
-
-const some1 = 'str1';
-
-class Person {}
--- less-003--class/src/app2.ts
+++
@@ -1,3 +0,0 @@
-console.log('Lesson 003 app2');
-
-const some2 = 'str2';
