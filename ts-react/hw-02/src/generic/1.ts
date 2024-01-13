83d9fa9292bf932b33e710b571bf40fb954749da
IvanRuskevych <ivan.ruskevych@gmail.com>
2024-01-13 22:02:21 +0200

    hw-02

--- ts-react/hw-02/src/generic/1.ts
+++ ts-react/hw-02/src/generic/1.ts
@@ -1,17 +1,31 @@
-// /*
-//   Є функція getPromise(), яка повертає проміс, що дозволяється в масив, що містить рядки та числа.
-//   Доповніть цю функцію, використовуючи generics, щоб вона повертала правильний тип.
-// */
+/*
+  Є функція getPromise(), яка повертає проміс, що дозволяється в масив, що містить рядки та числа. 
+  Доповніть цю функцію, використовуючи generics, щоб вона повертала правильний тип.
+*/
 
-// function getPromise () {
-//   return new Promise((resolve) => {
-//     resolve(['Text', 50]);
-//   });
-// }
+// 1) порядок елементів у масиві не важливий конкретний
 
-// getPromise()
-// .then((data) => {
-//   console.log(data);
-// });
+function getPromise1(): Promise<(number | string | boolean | null | undefined)[]> {
+  return new Promise((resolve) => {
+    resolve(['Text', 50, 'Text']);
+  });
+}
 
-// export {};
+getPromise1().then((data) => {
+  console.log(data);
+});
+
+// 2) конкретний порядок елементів у масиві важливий (спершу рядок, потім число)
+
+function getPromise2(): Promise<[number, string, string, boolean, null, undefined]> {
+  return new Promise((resolve) => {
+    resolve([50, 'Text', 'Text', false, null, undefined]);
+    // resolve(['Text', 50]); // error
+  });
+}
+
+getPromise2().then((data) => {
+  console.log(data);
+});
+
+export {};
--- ts-react/hw-02/src/generic/2.ts
+++ ts-react/hw-02/src/generic/2.ts
@@ -1,23 +1,38 @@
-// /*
-//   У вас є тип AllType. Існує функція compare, яка приймає два об'єкти. Ці об'єкти містять поля AllType.
-//   Ваше завдання – використовувати Pick та generics для вказівки, що поля цих об'єктів належать AllType.
-//   Функція compare повинна повертати AllType.
-// */
+/*
+  У вас є тип AllType. Існує функція compare, яка приймає два об'єкти. Ці об'єкти містять поля AllType. 
+  Ваше завдання – використовувати Pick та generics для вказівки, що поля цих об'єктів належать AllType.
+  Функція compare повинна повертати AllType.
+*/
 
-// type AllType = {
-//   name: string;
-//   position: number;
-//   color: string;
-//   weight: number
-// }
+type AllType = {
+  name: string;
+  position: number;
+  color: string;
+  weight: number;
+};
 
-// function compare (top, bottom): AllType {
-//   return {
-//     name: top.name,
-//     color: top.color,
-//     position: bottom.position,
-//     weight: bottom.weight,
-//   }
-// }
+type typeTop = Pick<AllType, 'name' | 'color'>;
+type typeBottom = Pick<AllType, 'position' | 'weight'>;
 
-// export {};
+function compare1(top: typeTop, bottom: typeBottom): AllType {
+  return {
+    name: top.name,
+    color: top.color,
+    position: bottom.position,
+    weight: bottom.weight,
+  };
+}
+
+function compare2<T extends AllType>(
+  top: Pick<T, keyof typeTop>,
+  bottom: Pick<T, keyof typeBottom>
+): AllType {
+  return {
+    name: top.name,
+    color: top.color,
+    position: bottom.position,
+    weight: bottom.weight,
+  };
+}
+
+export {};
